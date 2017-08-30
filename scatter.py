13ba26ee7d554daeb9a5b48163cdcdf1690c8032
Arjun Gupta <saimzn4241@gmail.com>
2017-08-30 23:39:03 +0530

    list property

--- scatter.py
+++ scatter.py
@@ -4,14 +4,21 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
 
 import random
 
 class ScatterTextWidget(BoxLayout):
+    text_color = ListProperty([1,0,0,1])
     def change_label_color(self, *args):
         color = [random.random() for i in xrange(3)] + [1]
-        label = self.ids['my_label']
-        label.color = color
+        # label = self.ids['my_label']
+        # label.color = color
+        # label1 = self.ids.label1
+        # label2 = self.ids.label2
+        # label1.color = color
+        # label2.color = color
+        self.text_color = color
 
 class ScatterApp(App):
     def build(self):
